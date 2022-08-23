479b180a50a14c2f2f10def92cb557ccf34ca9d3
Finn818 <jsolomons239@gmail.com>
2022-08-23 09:36:26 +0200

    addings 4

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-//Import libraries
+require('dotenv').config();
+const db = require("./config/dbconn");
 const express = require("express"); 
 const cors = require("cors");
+const bodyParser = require('body-parser');
+const conn = require("./config/dbconn");
 
-//Import routes
 const userRoute = require("./routes/userRoute");
 
-//Configurs Server
 const app = express();
 app.set("port", process.env.PORT || 6969);
 app.use(express.json());
 app.use(cors());
 
-// This is where I check URLs and Request methods to create functionality
-// GET '/' is always what will be displayed on the home page of your application
 app.get("/", (req, res) => {
     res.json({ msg: "Welcome" });
 });
 
-//Use individual routes when visiting these URLS
 app.use("/users", userRoute);
 
-//Set up Server to start listening for requests
 app.listen(app.get("port"), () => {
     console.log(`Listening for calls on port ${app.get("port")}`);
     console.log("Press Ctrl+C to exit server");
