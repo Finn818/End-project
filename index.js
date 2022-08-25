4fd11b430904cb6a37c228ef67f373cabc54d429
Finn818 <jsolomons239@gmail.com>
2022-08-25 16:09:20 +0200

    Updates

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const nodemailer = require('nodemailer');
 
 const userRouter = require("./routes/userRoutes");
 const productsRouter = require("./routes/productsRoutes");
-const productsRouter = require("./routes/productdetailsRouter");
+const productDetailsRouter = require("./routes/productdetailsRoutes");
 
 const app = express();
 app.set("port", process.env.PORT || 6969);
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 });
 app.use("/users", userRouter);
 app.use("/products", productsRouter);
-app.use("/productdetails", productdetailsRouter);
+app.use("/productDetails", productDetailsRouter);
 app.listen(app.get("port"), () => {
     console.log(`Listening for calls on port ${app.get("port")}`);
     console.log("Press Ctrl+C to exit server");
