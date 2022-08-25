6b0a55c52ba3264eb46329ee79dc4aed4ebf4146
Finn818 <jsolomons239@gmail.com>
2022-08-25 15:37:35 +0200

    Updates

--- routes/productDetails.js
+++ routes/productDetails.js
@@ -4,28 +4,19 @@ const db = require('../config/dbconn');
 const app = express();
 const router = express.Router();
 
-// router.get("/productDetails", bodyParser.json(), (req, res) => {
-//     try {
-//       db.query("SELECT * FROM productDetails;", (err, result) => {
-//         if (err) throw err;
-//         res.status(200).json({
-//           results: result
-//         });
-//       });
-//     } catch (error) {
-//       res.status(400).send(error.message);
-//     }
-//   });
-
-
-
-
-
-
-
-
-
-
-
+router.get('/',(req,res) => {
+    let products = `SELECT * FROM productDetails`
+    db.query(products,(err,results) => {
+        if(err){
+            console.log(err)
+            res.redirect('/error')
+        }else{
+            res.json({
+                status: 200,
+                products : results
+            })
+        }
+    })
+})
 
   module.exports = router;
