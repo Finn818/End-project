76edc80d70cccceae77a6a8f53b668543ab1ffad
Finn818 <jsolomons239@gmail.com>
2022-08-25 14:34:58 +0200

    Updates

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,16 +5,28 @@ const app = express();
 const router = express.Router();
 
 // Products functionalities
-app.get('/products', bodyParser.json(), (req, res)=> {
-    const strQry = `
-    SELECT id, Img_URL, prodName, prodPrice, prodArrival_Date,
-    Stock_Available, prodDesc
-    FROM products;
-    `
-    db.query(strQry, (err, results)=> {
-        if(err) throw err;
+// app.get('/products', bodyParser.json(), (req, res)=> {
+//     const strQry = `
+//     SELECT id, Img_URL, prodName, prodPrice, prodArrival_Date,
+//     Stock_Available, prodDesc
+//     FROM products;
+//     `
+//     db.query(strQry, (err, results)=> {
+//         if(err) throw err;
+//         res.status(200).json({
+//             results: results
+//         })
+//     })
+// });
+router.get("/products", bodyParser.json(), (req, res) => {
+    try {
+      db.query("SELECT * FROM products;", (err, result) => {
+        if (err) throw err;
         res.status(200).json({
-            results: results
-        })
-    })
-});
+          results: result
+        });
+      });
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  });
