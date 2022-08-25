3c5707ca99e97d68e1745d28e2c5996bdcebeebd
Finn818 <jsolomons239@gmail.com>
2022-08-25 14:54:53 +0200

    Updates

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,19 +5,19 @@ const app = express();
 const router = express.Router();
 
 // Products functionalities
-// app.get('/products', bodyParser.json(), (req, res)=> {
-//     const strQry = `
-//     SELECT id, Img_URL, prodName, prodPrice, prodArrival_Date,
-//     Stock_Available, prodDesc
-//     FROM products;
-//     `
-//     db.query(strQry, (err, results)=> {
-//         if(err) throw err;
-//         res.status(200).json({
-//             results: results
-//         })
-//     })
-// });
+app.get('/products', bodyParser.json(), (req, res)=> {
+    const strQry = `
+    SELECT id, Img_URL, prodName, prodPrice, prodArrival_Date,
+    Stock_Available, prodDesc
+    FROM products;
+    `
+    db.query(strQry, (err, results)=> {
+        if(err) throw err;
+        res.status(200).json({
+            results: results
+        })
+    })
+});
 
 
   module.exports = router;
