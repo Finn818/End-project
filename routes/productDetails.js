551740bb08ab8344dded36357a2ffcd9db3f8844
Finn818 <jsolomons239@gmail.com>
2022-08-25 15:14:11 +0200

    Updates

--- routes/productDetails.js
+++ routes/productDetails.js
@@ -17,36 +17,7 @@ const router = express.Router();
 //     }
 //   });
 
-// SHOW ALL PRODUCTS
-router.get('/products',(req,res) => {
-    let products = `SELECT * FROM products`
-    con.query(products,(err,results) => {
-        if(err){
-            console.log(err)
-            res.redirect('/error')
-        }else{
-            res.json({
-                status: 200,
-                products : results
-            })
-        }
-    })
-})
-// SHOW SINGLE PRODUCT
-router.get('/products/:id',(req,res) => {
-    let products = `SELECT * FROM products WHERE ID = ${req.params.id};`;
-    con.query(products,(err,results) => {
-        if(err){
-            console.log(err)
-            res.redirect('/error')
-        }else{
-            res.json({
-                status: 200,
-                product : results
-            })
-        }
-    })
-})
+
 
 
 
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -4,21 +4,36 @@ const db = require('../config/dbconn');
 const app = express();
 const router = express.Router();
 
-// Products functionalities
-app.get('/products', bodyParser.json(), (req, res)=> {
-    const strQry = `
-    SELECT id, Img_URL, prodName, prodPrice, prodArrival_Date,
-    Stock_Available, prodDesc
-    FROM products
-    ;
-    `;
-    db.query(strQry, (err, results)=> {
-        if(err) throw err;
-        res.status(200).json({
-            results: results
-        })
+// SHOW ALL PRODUCTS
+router.get('/products',(req,res) => {
+    let products = `SELECT * FROM products`
+    con.query(products,(err,results) => {
+        if(err){
+            console.log(err)
+            res.redirect('/error')
+        }else{
+            res.json({
+                status: 200,
+                products : results
+            })
+        }
     })
-});
+})
+// SHOW SINGLE PRODUCT
+router.get('/products/:id',(req,res) => {
+    let products = `SELECT * FROM products WHERE ID = ${req.params.id};`;
+    con.query(products,(err,results) => {
+        if(err){
+            console.log(err)
+            res.redirect('/error')
+        }else{
+            res.json({
+                status: 200,
+                product : results
+            })
+        }
+    })
+})
 
 
   module.exports = router;
