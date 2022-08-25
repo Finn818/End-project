03644e5f7bf9a1e8e9639942bf88178a5638a9fe
Finn818 <jsolomons239@gmail.com>
2022-08-25 15:27:19 +0200

    Updates

--- index.js
+++ index.js
@@ -22,38 +22,41 @@ express.urlencoded( {
 app.get("/", (req, res) => {
     res.status(200).json({ msg: "Welcome" });
 });
-app.use("/products", productsRouter);
 app.use("/users", userRouter);
+app.use("/products", productsRouter);
 app.listen(app.get("port"), () => {
     console.log(`Listening for calls on port ${app.get("port")}`);
     console.log("Press Ctrl+C to exit server");
 });
 
 // ============================================
-// Products functionalities
-// app.get('/products', (req, res)=> {
-//     const strQry = `
-//     SELECT id, Img_URL, prodName, prodPrice, prodSize, prodArrival_Date, Stock_Available, prodDesc 
-//     FROM products;
-//     `
-//     db.query(strQry, (err, results)=> {
-//         if(err) throw err;
-//         res.status(200).json({
-//             results: results
-//         })
-//     })
-// });
-
-// app.get('/products/:id', (req, res)=> {
-//     const strQry = `
-//     SELECT id, Img_URL, prodName, prodPrice, prodSize, prodArrival_Date, Stock_Available, prodDesc
-//     FROM products
-//     WHERE id = ?,?,?,?,?,?,?;
-//     `
-//     db.query(strQry, [req.params.id], (err, results)=> {
-//         if(err) throw err;
-//         res.status(200).json({
-//             results: results
-//         })
-//     })
-// });
+// SHOW ALL PRODUCTS
+router.get('/products',(req,res) => {
+    let products = `SELECT * FROM products`
+    con.query(products,(err,results) => {
+        if(err){
+            console.log(err)
+            res.redirect('/error')
+        }else{
+            res.json({
+                status: 200,
+                products : results
+            })
+        }
+    })
+})
+// SHOW SINGLE PRODUCT
+router.get('/products/:id',(req,res) => {
+    let products = `SELECT * FROM products WHERE ID = ${req.params.id};`;
+    con.query(products,(err,results) => {
+        if(err){
+            console.log(err)
+            res.redirect('/error')
+        }else{
+            res.json({
+                status: 200,
+                product : results
+            })
+        }
+    })
+})
