e7c5315a2468f54c0e4676d5f1421d306e04f06a
Finn818 <jsolomons239@gmail.com>
2022-09-09 12:10:45 +0200

    Updates

---
+++ routes/fullproductDetailsRouter.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('../config/dbconn');
+const app = express();
+const router = express.Router();
+
+   // SHOW FullProductDetails PRODUCT
+   router.get('/',(req,res) => {
+    let FullProductDetails = `SELECT * FROM FullProductDetails`;
+    db.query(FullProductDetails, [req.params.id],(err,results) => {
+        if(err) res.status(400).json({msg: "no shoes in stock" })
+        res.json({
+            status: 200,
+            FullProductDetails : results
+            })
+        })
+    })
+
+
+module.exports = router;
