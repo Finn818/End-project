931aac0ca3c41fffdd4510d45cfbd99d03f48074
Finn818 <jsolomons239@gmail.com>
2022-09-09 09:38:27 +0200

    Updates

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -31,17 +31,15 @@ router.get('/:id',(req,res) => {
         })
     })
 
-    router.get('/id',(req,res) => {
-        let FullProductDetails = `SELECT * FROM FullProductDetails`
-        db.query(FullProductDetails,(err,results) => {
-            if(err){
-            res.status(400).json({msg: err});
-            }else{
-            res.json({
+    // SHOW FullProductDetails PRODUCT
+router.get('/:id',(req,res) => {
+    let FullProductDetails = `SELECT * FROM FullProductDetails WHERE id = ?;`;
+    db.query(FullProductDetails, [req.params.id],(err,results) => {
+        if(err) res.status(400).json({msg: "no shoes in stock" })
+        res.json({
             status: 200,
             FullProductDetails : results
-                })
-            }
+            })
         })
     })
 
